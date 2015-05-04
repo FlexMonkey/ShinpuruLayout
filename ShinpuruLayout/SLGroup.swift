befef0738be118d3135594b610228a785e25377d
Simon Gladman <sgladman@gmail.com>
2015-05-04 14:40:28 +0100

    Added GNU General Public License

--- ShinpuruLayout/SLGroup.swift
+++ ShinpuruLayout/SLGroup.swift
@@ -5,11 +5,18 @@
 //  Created by Simon Gladman on 02/05/2015.
 //  Copyright (c) 2015 Simon Gladman. All rights reserved.
 //
-// TODO 
-//  Create suite of UI components that implement SLLayoutItem
-//  Support for alignment
-//  Explicit width / height 
-//  Spacer component
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>
 
 import UIKit
 
@@ -22,6 +29,7 @@ class SLHGroup: SLGroup
 
         for (index: Int, child: UIView) in enumerate(children)
         {
+
             let percentageWidth = childPercentageSizes[index] / 100 * (frame.width - totalExplicitSize)
             let componentWidth: CGFloat = hasExplicitSize(child) ? (child as! SLLayoutItem).explicitSize! : percentageWidth
             
